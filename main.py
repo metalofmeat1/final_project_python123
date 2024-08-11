8716f44c49983cf966b5843bf0b56ac59d7f7ae9
Vova <vovinator2010@gmail.com>
2024-08-11 11:02:08 +0300

    Тут оновлений main.py з двома html, в одній з яких є пошукова панель - результат у змінній під назвою search_query

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 print("Hello, PYTHON_1y_21_16_09_23_3")
 print("Вова тут :)")
+from flask import Flask, render_template, request, flash, redirect, url_for
+app = Flask(__name__)
+app.secret_key = 'secret_key'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    search_query = request.form.get('query')
+    return render_template('search.html', query=search_query)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
