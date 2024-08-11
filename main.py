239ab509bfa125ed10e297cb4b75c0c743c0f251
yevfff <yevfimiiloboda1276@gmail.com>
2024-08-11 10:44:33 +0300

    створив структуру та базу

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-print("Hello, PYTHON_1y_21_16_09_23_3")
-print("Вова тут :)")
-print('Фімочка тут')
-print('Маша тут')
-print("Рома тут ;)")
+from flask import Flask, render_template, jsonify
+
+
+app = Flask(__name__)
+
+test_data = {
+  "events": [
+    {
+      "year": 1000,
+    #   Координати мітки.
+      "location": [50.4501, 30.5234], 
+      "title": "Тест",
+      "description": "Опис",
+    #   Можна буде прикріпити картинки або відео, якщо в майбутньому це буде потрібно 
+      "media": {
+        "image": "battle_kyiv.jpg",
+        "video": "battle_kyiv.mp4"
+
+      }
+    }
+  ]
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/data')
+def get_data():
+    return jsonify(test_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
