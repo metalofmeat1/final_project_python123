f810dbeb01d10d24895bb5eea7397b6bb933177a
yevfff <yevfimiiloboda1276@gmail.com>
2024-08-30 22:46:10 +0300

    Додав пожливість додавати, редагувати та видаляти постатей з галереї

--- init_db.py
+++ init_db.py
@@ -1,19 +1,13 @@
 import sqlite3
 import os
 
-
 def init_db():
     db_file = 'historical_figures.db'
 
-    # Видалити існуючий файл бази даних
-    if os.path.exists(db_file):
-        os.remove(db_file)
 
-    # Створення нової бази даних та таблиць
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
 
-    # Створення таблиці постатей
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS figures (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,33 +16,21 @@ def init_db():
         death_year INTEGER,
         biography TEXT,
         notable_for TEXT,
-        image_filename TEXT
+        image BLOB
     )
     ''')
 
-    # Створення таблиці каруселі
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS carousel (
+    CREATE TABLE IF NOT EXISTS figure_detail (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        figure_id INTEGER,
-        image_filename TEXT NOT NULL,
-        FOREIGN KEY (figure_id) REFERENCES figures (id)
+        figure_id INTEGER NOT NULL,
+        detail TEXT,
+        FOREIGN KEY (figure_id) REFERENCES figures(id)
     )
     ''')
-
-    # Створення таблиці деталей постатей
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS figure_detail (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            figure_id INTEGER NOT NULL,
-            detail TEXT,
-            FOREIGN KEY (figure_id) REFERENCES figures(id)
-        )
-        ''')
 
     conn.commit()
     conn.close()
-
 
 if __name__ == '__main__':
     init_db()
